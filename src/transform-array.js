8f1ca24c5a6d7aeb84d009dd95a48fec32f192bf
Antonina Dubich <dubich.t@gmail.com>
2020-09-24 00:04:41 +0300

    fix: implement correct function transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,27 +1,30 @@
 const CustomError = require("../extensions/custom-error");
-let newArray = [];
-if (Array.isArray(arr)) {
 
-  newArray = arr.slice()
+module.exports = function transform(arr) {
+  let newArray = [];
+  if (Array.isArray(arr)) {
 
-  for(let i = 0; i < newArray.length; i++ ){
+    newArray = arr.slice()
+
+    for(let i = 0; i < newArray.length; i++ ){
+      
+      if(newArray[i] === '--discard-prev'){
+        if(i === 0) {
+          break;
+        }
+        newArray.splice(i - 1, 1)
+      } else if (newArray[i] === '--discard-next'){
+        newArray.splice(i + 1, 1)
+      } else if (newArray[i] === '--double-prev'){
+        newArray.splice(i, 1, newArray[i - 1])
+      } else if (newArray[i] === '--double-next'){
+        newArray.splice(i, 1, newArray[i + 1])
+      } 
+    }
     
-    if(newArray[i] === '--discard-prev'){
-      if(i === 0) {
-        break;
-      }
-      newArray.splice(i - 1, 1)
-    } else if (newArray[i] === '--discard-next'){
-      newArray.splice(i + 1, 1)
-    } else if (newArray[i] === '--double-prev'){
-      newArray.splice(i, 1, newArray[i - 1])
-    } else if (newArray[i] === '--double-next'){
-      newArray.splice(i, 1, newArray[i + 1])
-    } 
+    newArray = newArray.filter(item => (item !== '--discard-prev' && item !== '--discard-next' && item !== '--double-prev'&& item !== '--double-next'&& item != null && item !== undefined)) 
+    return newArray;
+  } else {
+    throw new Error;
   }
-  
-  newArray = newArray.filter(item => (item !== '--discard-prev' && item !== '--discard-next' && item !== '--double-prev'&& item !== '--double-next'&& item != null && item !== undefined)) 
-  return newArray;
-} else {
-  throw new Error;
 };
