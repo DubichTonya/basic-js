b6d58a9216b351359619e2911df6ef997f9801fe
Antonina Dubich <dubich.t@gmail.com>
2020-09-23 23:31:31 +0300

    feat: add function transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
+let newArray = [];
+if (Array.isArray(arr)) {
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  newArray = arr.slice()
+
+  for(let i = 0; i < newArray.length; i++ ){
+    
+    if(newArray[i] === '--discard-prev'){
+      if(i === 0) {
+        break;
+      }
+      newArray.splice(i - 1, 1)
+    } else if (newArray[i] === '--discard-next'){
+      newArray.splice(i + 1, 1)
+    } else if (newArray[i] === '--double-prev'){
+      newArray.splice(i, 1, newArray[i - 1])
+    } else if (newArray[i] === '--double-next'){
+      newArray.splice(i, 1, newArray[i + 1])
+    } 
+  }
+  
+  newArray = newArray.filter(item => (item !== '--discard-prev' && item !== '--discard-next' && item !== '--double-prev'&& item !== '--double-next'&& item != null && item !== undefined)) 
+  return newArray;
+} else {
+  throw new Error;
 };
