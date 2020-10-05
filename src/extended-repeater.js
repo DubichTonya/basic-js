9248a3a49babef2ee5a61203b9dfdca1b35c3a3c
Antonina Dubich <dubich.t@gmail.com>
2020-10-05 20:23:27 +0300

    feat add function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,77 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+  if(options.separator === undefined){
+    options.separator = '+'
+  } else if(options.additionSeparator === undefined) {
+    options.additionSeparator = '|'
+  }
+  let result;
+
+  if (typeof (str) !== 'string') {
+    str = str + '';
+  }
+
+  if (options.repeatTimes) {
+    result = [];
+    
+
+    if (`${options.addition}` && options.addition !== undefined) {
+      let substr;
+
+      if (options.additionRepeatTimes) {
+        substr = [];
+
+        for (let i = 0; i < options.additionRepeatTimes; i++) {
+          substr.push(`${options.addition}`)
+        }
+
+        for (let j = 0; j < substr.length - 1; j++) {
+          substr[j] += options.additionSeparator;
+        }
+
+        substr = substr.join('')
+        // console.log(substr)
+      } else {
+        substr = options.addition;
+      }
+      str += substr;
+    } 
+  
+    for(let k = 0; k < options.repeatTimes; k ++) {
+      result.push(str)
+    }
+
+    for(let n = 0; n < options.repeatTimes - 1; n ++) {
+      result[n] += options.separator
+    }
+    result = result.join('')
+  } else {
+    if (options.addition) {
+      let substr;
+
+      if (options.additionRepeatTimes) {
+        substr = [];
+
+        for (let i = 0; i < options.additionRepeatTimes; i++) {
+          substr.push(options.addition)
+        }
+
+        for (let j = 0; j < substr.length - 1; j++) {
+          substr[j] += options.additionSeparator;
+        }
+
+        substr = substr.join('')
+        
+      } else {
+        substr = options.addition;
+      }
+      str += substr;
+    } 
+    result = str;
+  }
+
+  return result;
 };
   
