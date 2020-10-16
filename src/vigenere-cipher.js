7b5f73b32b38e1d04c9ae27b48b12f782fe98349
Antonina Dubich <dubich.t@gmail.com>
2020-10-17 01:51:43 +0300

    add function decrypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -55,9 +55,46 @@ class VigenereCipheringMachine {
   }
 
 
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str, key) {
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase();
+    str = str.toLowerCase()
+    key = key.toLowerCase();
+    
+    let space = {};
+    str.split('').map((item, index) => {
+      if (!(item.charCodeAt(0) >= 97 && item.charCodeAt(0) <= 122)){
+        space[index] = item;
+        return space;
+      }
+    });
+    
+    let subStr = str.match(/\w/g);
+    let keyCode = this.codeGenerator(subStr, key)
+    let strTemp = [] 
+    let codeTemp = []
+    let resArr = [];
+    
+    for(let i = 0; i < str.length; i++){
+      if(alphabet.indexOf(str[i]) !== -1) {
+        strTemp.push(alphabet.indexOf(str[i]))
+      }
+    }
+    
+    for(let i = 0; i < keyCode.length; i++){
+      if(alphabet.indexOf(keyCode[i]) !== -1) {
+        codeTemp.push(alphabet.indexOf(keyCode[i]))
+      }
+    }
+
+    for(let i = 0; i < keyCode.length; i++){
+      resArr.push(alphabet[Math.abs((codeTemp[i] - strTemp[i]-26)%26)])
+    }
+  
+    for(key in space){
+      resArr.splice(key, 0, space[key])
+    }
+    
+    return resArr.join('').toUpperCase();
   }
 }
 
